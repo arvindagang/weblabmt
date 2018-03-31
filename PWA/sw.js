94eefbe1bc8769f5243ce1576e7fc8e5a7fd7d4f
2167856 <2167856@slu.edu.ph>
2018-03-31 18:58:04 +0800

    Added service worker js and navigation bar for PWA

---
+++ PWA/sw.js
@@ -0,0 +1,44 @@
+var cacheName = "offline";
+
+/**
+ * Install event
+ */
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll([
+                "/index.html",
+                "/navbar.css",
+                "/navbar.js",
+                "/memo.css",
+                "/memo.html",
+                "/memo.js"
+            ]);
+        })
+    )
+})
+
+self.addEventListener("activate", function(event){
+    event.waitUntil(
+        caches.keys().then(function(names){
+            Promise.all(
+                name.filter(function(name){
+                    return name !== cacheName;
+                }).map(function(name){
+                    return caches.delete(name);
+                })
+            )
+        })
+    )
+});
+
+/**
+ * 
+ */
+self.addEventListener("fecth", function(event){
+    event.respondWith(
+        caches.open(cacheName).then(function(cache){
+            return cache.match(event.request);
+        })
+    );
+});
