57bab44fc9699d29f267159d9f9758f5e2972be6
2167856 <2167856@slu.edu.ph>
2018-04-09 02:23:11 +0800

    Fixed bugs regarding the memo feature

--- PWA/memo.js
+++ PWA/memo.js
@@ -1,60 +1,118 @@
-var check = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
-var trash = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
-
-
+var memo = [];
+var data3 = []
+var ctr = 0;
+getMemo();
 /**
- * list, paid, unpaid, reminder
- * buton = 'add'
+ * Method will get the data that is available inside the local storage
  */
-
-document.getElementById('add').addEventListener('click', function () {
-    var value = document.getElementById('reminder').value;
-    if (value) addItemList(value);
-});
-
-function removeItem(remove) {
-    var item = this.parentNode.parentNode;
-    var parent = item.parentNode;
-    parent.removeChild(item);
-}
-
-function paidItem(paid){
-    var item = this.parentNode.parentNode;
-    var parent = item.parentNode;
-    var id = parent.id;
-
-    var target = (id === 'todo') ? document.getElementById('paid'):document.getElementById('unpaid');
-    if(id === 'unpaid'){
-        target = document.getElementById('paid');
-    }else{
-        target = document.getElementById('unpaid');
+function getMemo() {
+    memo = JSON.parse(localStorage.getItem('memo'));
+    if (memo) {
+        for (var i = 0; i < memo.length; i++) {
+            var amount = memo[i].amount;
+            var dueDate = memo[i].dueDate;
+            var org = memo[i].org;
+            addItemList(amount, dueDate, org);
+        }
+    } else {
+        memo = [];
     }
 }
 
-function addItemList(text) {
+
+
+/**
+ * Store data in the localStorage whenever a new information is send by the user
+ */
+document.getElementById('add').addEventListener('click', function () {
+    var amount = document.getElementById('amount').value;
+    var dueDate = document.getElementById('dueDate').value;
+    var org = document.getElementById('org').value;
+
+    data = {
+        amount: amount,
+        dueDate: dueDate,
+        org: org
+    }
+    memo.push(data);
+    localStorage.setItem('memo', JSON.stringify(memo));
+
+    if (amount, dueDate, org) addItemList(amount, dueDate, org);
+});
+
+/**
+ * 
+ * @param {*} remove 
+ * Remove a certain item list from the localStorage
+ */
+function removeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    for (var i = 0; i < data3.length; i++) {
+
+        if (data3[i].item === item){
+            memo.splice(i, 1);
+            data3.splice(i, 1);
+            localStorage.setItem('memo', JSON.stringify(memo));
+            break;
+        }
+    }
+    parent.removeChild(item);
+}
+
+
+
+/**
+ * Add items
+ * @param {*} amount 
+ * @param {*} dueDate 
+ * @param {*} org 
+ */
+function addItemList(amount, dueDate, org) {
+    var currentDay = new Date();
+    var d = new Date(dueDate);
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    var mm = months[d.getUTCMonth()];
     var list = document.getElementById('unpaid');
     var item = document.createElement('li');
-    item.innerText = text;
+    item.innerHTML = "Due Date: " + mm + " " + d.getUTCDate() + ", " + d.getUTCFullYear() + " <br>Type: " + org + "<br> " + "Amount: " + amount;
+    var due = d.getUTCDate() - currentDay.getUTCDate();
+    var remind = document.createElement('div');
+
+    var data2 = {
+        item: item,
+        amount: amount,
+        dueDate: dueDate,
+        org: org
+    }
+    data3.push(data2);
+
+
+    if (due <= 7 && due > 0) {
+        remind.innerHTML = "<p> You have " + due + " days left </p>";
+        item.appendChild(remind);
+    } else if (due === 0) {
+        remind.innerHTML = "<p> You need to pay now </p>";
+        item.appendChild(remind);
+    } else if (due < 0) {
+        remind.innerHTML = "<p> You are overdue </p>";
+        item.appendChild(remind);
+    }
+
+
 
     var buttons = document.createElement('div');
     buttons.classList.add('buttons');
-
     var remove = document.createElement('button');
     remove.classList.add('remove');
-    remove.innerHTML = "<button> Paid </button>"
+    remove.innerHTML = "Paid";
 
     remove.addEventListener('click', removeItem);
-
-    var paid = document.createElement('button');
-    paid.classList.add('paid');
-    paid.innerHTML = check;
-    
-    paid.addEventListener('click', paidItem);
-
     buttons.appendChild(remove);
-    buttons.appendChild(paid);
+
     item.appendChild(buttons);
 
     list.appendChild(item);
     list.insertBefore(item, list.childNodes[0]);
+
 }
