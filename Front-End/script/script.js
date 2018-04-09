90492912af9bc5935d12920c694cad619e074ef1
Bernabe Casey <2162103@slu.edu.ph>
2018-04-10 03:10:09 +0800

    Added the maps for possible electricity payment centers

---
+++ Front-End/script/script.js
@@ -0,0 +1,22 @@
+
+            function showelec(){
+                document.getElementById('water1').style.display = 'none';
+                document.getElementById('water2').style.display = 'none';
+                document.getElementById('elec1').style.display = 'block';
+                document.getElementById('elec2').style.display = 'block'; 
+            }
+            function showwater(){
+                document.getElementById('elec1').style.display = 'none';
+                document.getElementById('elec2').style.display = 'none'; 
+                document.getElementById('water1').style.display = 'block';
+                document.getElementById('water2').style.display = 'block';
+            }
+            function showmap1(){
+                document.getElementById('map2').style.display = 'none';
+                document.getElementById('map1').style.display = 'block';
+                document.getElementById('map').style.display = 'block';
+            }function showmap2(){
+                document.getElementById('map1').style.display = 'none';
+                document.getElementById('map2').style.display = 'block';
+                document.getElementById('map').style.display = 'block';
+            }
